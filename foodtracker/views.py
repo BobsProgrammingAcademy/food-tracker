69216bbe77be4ffb4ff06be22febbae5276265a5
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2022-04-01 16:09:53 +0100

    Updated views

--- foodtracker/views.py
+++ foodtracker/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
