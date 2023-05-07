8cc4c836ebf8a3632760ae1bce80a45fe18f4be3
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-07 12:19:14 +0200

    Updated unit tests

--- foodtracker/tests.py
+++ foodtracker/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User
+from .models import User, FoodCategory
 
 
 class UserModelUnitTestCase(TestCase):
@@ -11,3 +11,12 @@ class UserModelUnitTestCase(TestCase):
         self.assertTrue(isinstance(data, User))
         self.assertIsInstance(data, User)
         self.assertEqual(str(data.username), 'John')
+
+
+class FoodCategoryUnitTestCase(TestCase):
+    def setUp(self):
+        self.category = FoodCategory.objects.create(category_name='Vegetables')
+
+    def test_food_category_model(self):
+        data = self.category
+        self.assertIsInstance(data, FoodCategory)
