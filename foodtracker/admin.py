214308e77c371aa481bafe927bc0e6952c0b9c27
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-12-01 12:18:25 +0100

    Code refactoring

--- foodtracker/admin.py
+++ foodtracker/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import User, Food, FoodCategory, FoodLog, Image, Weight
 
+
 admin.site.register(User)
 admin.site.register(Food)
 admin.site.register(FoodCategory)
